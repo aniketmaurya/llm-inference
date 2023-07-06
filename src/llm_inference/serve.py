6297f8846e3fe1a02b7372a3a17da390bfa10997
Aniket Maurya <aniket@lightning.ai>
2023-07-06 08:13:01 -0400

    fixes

--- src/llm_inference/serve.py
+++ src/llm_inference/serve.py
@@ -15,33 +15,30 @@ class Response(BaseModel):
     result: str
 
 
-class ServeLLaMA(PythonServer):
+class ServeLitGPT(PythonServer):
     def __init__(
         self,
         input_type,
         output_type,
-        checkpoint_path: str = None,
-        tokenizer_path: str = None,
+        checkpoint_dir: str = None,
     ):
         super().__init__(input_type, output_type)
-        self.checkpoint_path = checkpoint_path
-        self.tokenizer_path = tokenizer_path
+        self.checkpoint_dir = checkpoint_dir
 
     def setup(self, *args: Any, **kwargs: Any) -> None:
         self._model = LLMInference(
-            checkpoint_path=self.checkpoint_path,
-            tokenizer_path=self.tokenizer_path,
-            dtype="bfloat16",
+            checkpoint_dir=self.checkpoint_dir,
         )
 
     def predict(self, request: PromptRequest) -> Any:
-        result = self._model(request.prompt)
+        result = self._model.chat(request.prompt)
         return Response(result=result)
 
 
 if __name__ == "__main__":
-    component = ServeLLaMA(
+    component = ServeLitGPT(
         input_type=PromptRequest,
         output_type=Response,
+        checkpoint_dir="examples/chatbot/checkpoints/lmsys/longchat-7b-16k/"
     )
     app = L.LightningApp(component)
